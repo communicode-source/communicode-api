f3c5d93d85dfb9470c2f4584fc7abf70ecd267bb
cooper-campbell <cooper.lax98@gmail.com>
2017-01-23 17:06:16 -0500

    Got rid of some debugging code

--- app/middleware/genToken.js
+++ app/middleware/genToken.js
@@ -1,9 +1,18 @@
 var jwt = require('jwt-simple');
+//var User = require('./../models/User.js');
+
 
 var genToken = {
   generate: function(req, res) {
     // Validation here.
     res.status(200).json({"token": generateToken()});
+  },
+
+  userGenerate: function(req, res) {
+    /**
+    * Once the User object has been created then I will be good to go
+    *   to start making this upon login.
+    **/
   }
 }
 
@@ -18,7 +27,6 @@ function generateToken() {
 function expiresIn(numMin) {
   var dateObj = new Date();
   var expir = new Date(dateObj.getTime() + numMin*60000);
-  console.log(expir);
   return expir.getTime();
 }
 
