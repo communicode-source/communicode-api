efb59c0c62cb30a22350c175dc7db1ca87bde546
Joseph Gerardot <indyJAG@gmail.com>
2017-01-24 21:52:33 -0500

    Extra comment in Messages model

--- app/models/Messages.js
+++ app/models/Messages.js
@@ -1,5 +1,5 @@
 //A mongoose schema and model for messages
-
+//Joseph G
 var mongoose = require('mongoose'); //grab mongoose
 module.export = new mongoose.model('Message', { //assign to module.export (allows outside access), create a new model with a new schema inside holding the data
   to: { type:mongoose.Schema.Types.ObjectId, ref: 'Users' }, // syntax for mongoose Schema is name: Data type , {inside} for more tags and with each term separated by a comma
