279dd545a01be698b3f2f0dee1ee4233cdcc367d
Qwertycrackers <joseph@gerardot.org>
2017-01-24 20:17:33 -0500

    Minor correction to a comment and testing

--- app/models/Messages.js
+++ app/models/Messages.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose'); //grab mongoose
 module.export = new mongoose.model('Message', { //assign to module.export (allows outside access), create a new model with a new schema inside holding the data
-  to: { type:mongoose.Schema.Types.ObjectId, ref: 'Users' }, // syntax for mongoose documents is name: Data type , {inside} for more tags and with each term separated by a comma
+  to: { type:mongoose.Schema.Types.ObjectId, ref: 'Users' }, // syntax for mongoose Schema is name: Data type , {inside} for more tags and with each term separated by a comma
   from:{ type:mongoose.Schema.Types.ObjectId, ref: 'Users'},
   message: String,
   time: String
