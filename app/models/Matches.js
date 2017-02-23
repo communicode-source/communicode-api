be04201816c341a5dddc61e59aafc0c22fa15d0f
Daniel Adelfinsky <danila726@gmail.com>
2017-02-23 18:46:49 -0500

    Text error Fixes
    Matches was spelled wrong

--- app/models/Matches.js
+++ app/models/Matches.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 // Exports schema for matches object
-module.exports = mongoose.model('Mat', {
+module.exports = mongoose.model('Matches', {
   developer : {type: mongoose.Schema.Types.ObjectId, ref: 'Users',
   nonprofit : {type: mongoose.Schema.Types.ObjectId, ref: 'Users'},
   percentage: Integer
