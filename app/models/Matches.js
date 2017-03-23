f34dbc70555556bec9f66c5997f721738396dea6
Daniel Adelfinsky <danila726@gmail.com>
2017-03-22 20:22:37 -0400

    New

--- app/models/Matches.js
+++ app/models/Matches.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 
 // Exports schema for matches object
-module.exports = mongoose.model('Matches', {
-  developer : {type: mongoose.Schema.Types.ObjectId, ref: 'Users',
+module.exports = mongoose.model('Match', {
+  developer : {type: mongoose.Schema.Types.ObjectId, ref: 'Users'},
   nonprofit : {type: mongoose.Schema.Types.ObjectId, ref: 'Users'},
-  percentage: Integer
+  percentage: String
 });
--- app/routes/matches/index.js
+++ app/routes/matches/index.js
@@ -1,17 +1,42 @@
 const matches = require('express').Router();
 
-var Matches = require('./../../models/Matches');
+var Match = require('../../models/Matches.js');
 
-matches.route('/matches')
+matches.route('/')
 
   .get((req , res) => {
-    res.status(200).json({message: "Matches go here"});
+    Match.find({}, function(err, matches) {
+      res.status(200).json(matches);
+    });
+  })
+
+  .post((req,res) => {
+    var newMatch = new Match({
+      'developer' : req.body.developer,
+      'nonprofit' : req.body.nonprofit,
+      'percentage': req.body.percentage
+    });
+
+    newMatch.save((err, data) => {
+      if (err) throw err;
+
+      console.log("Saved: " + JSON.stringify(data));
+    });
   });
 
   matches.route('/:id')
 
     .get((req, res) => {
       var id = req.params.id;
-      res.status(200).json({ id: id, name: "Match"});
-    });
-module.exports = matches
+      Match.findById(id, function(err, matches) {
+        if (err)
+          res.status(500).json({"error": "Greeting with that ID does not exist"});
+
+        res.status(200).json(matches);
+      });
+    })
+
+
+
+    //  Match.findById(id, function()
+module.exports = matches;
